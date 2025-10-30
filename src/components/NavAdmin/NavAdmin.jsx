142f64908e1e1b5179392a2fada41d3ac9971a2b
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2025-10-30 14:07:27 -0600

    feat:Se agrego nav y formulario para vacantes

---
+++ src/components/NavAdmin/NavAdmin.jsx
@@ -0,0 +1,26 @@
+export const NavAdmin = () => {
+    return (
+
+            <div className="container ">
+                <div className="d-flex align-items-start">
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h3 className="text-start">My Company - Administración</h3>
+                    </div>
+                </div>
+                <div className="row bg-light mt-2">
+                    <div className="col-4">
+                        <button className="btn w-100">Crear Vacante</button>
+                    </div>
+                    <div className="col-4">
+                        <button className="btn w-100">Vacantes</button>
+                    </div>
+                    <div className="col-4">
+                        <button className="btn w-100">Salir</button>
+                    </div>
+                </div>
+            </div>
+
+    );
+};
